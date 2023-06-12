cff553963729320a653aa66c0a2746cd1e9b5560
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-13 00:38:53 +0200

    state & eventhandle in React

--- src/App.js
+++ src/App.js
@@ -1,33 +1,70 @@
+import { useState } from "react";
+import { message } from "./constants";
+
 const App = () => {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
-    <div>
-      <div className="w-[300px] m-auto flex flex-col items-center bg-red-100">
+    <div className="w-full h-[300px] flex justify-center items-center">
+      <div className="w-[300px] h-auto m-auto flex flex-col items-center border-[1px] bg-slate-50 shadow-md shadow-slate-300 rounded-md">
         {/**Steps */}
-        <div className=" px-2 py-5 w-[300px] flex justify-between bg-green-100">
-          <div className="w-10 h-10 bg-slate-200 flex justify-center items-center rounded-full">
+        <div className=" px-2 py-5 w-[300px] flex justify-between">
+          <div
+            className={
+              step >= 1
+                ? "w-10 h-10 font-medium text-white bg-purple-500 flex justify-center items-center rounded-full"
+                : "w-10 h-10 font-medium bg-slate-200 flex justify-center items-center rounded-full"
+            }
+          >
             1
           </div>
-          <div className="w-10 h-10 bg-slate-200 flex justify-center items-center rounded-full">
+          <div
+            className={
+              step >= 2
+                ? "w-10 h-10 font-medium text-white bg-purple-500 flex justify-center items-center rounded-full"
+                : "w-10 h-10 font-medium bg-slate-200 flex justify-center items-center rounded-full"
+            }
+          >
             2
           </div>
-          <div className="w-10 h-10 bg-slate-200 flex justify-center items-center rounded-full">
+          <div
+            className={
+              step >= 3
+                ? "w-10 h-10 font-medium text-white bg-purple-500 flex justify-center items-center rounded-full"
+                : "w-10 h-10 font-medium bg-slate-200 flex justify-center items-center rounded-full"
+            }
+          >
             3
           </div>
         </div>
 
         {/**Message */}
         <p className="mx-2 my-5">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
+          <span className="text-lg font-medium">Step{step}: </span>
+          {message[step - 1]}.
         </p>
 
         {/**Buttons */}
-        <div className="w-[300px] bg-pink-100">
-          <button className="px-2 py-2 text-white bg-purple-500 hover:bg-purple-700 rounded-lg">
+        <div className="px-2 my-5 w-[300px] flex justify-between ">
+          <button
+            onClick={handlePrevious}
+            className="w-24 px-2 py-2 text-white font-medium bg-purple-500 hover:bg-purple-700 rounded-lg"
+          >
             Previous
           </button>
 
-          <button className="px-2 py-2 text-white bg-purple-500 hover:bg-purple-700 rounded-lg">
+          <button
+            onClick={handleNext}
+            className="w-24 px-2 py-2 text-white font-medium bg-purple-500 hover:bg-purple-700 rounded-lg"
+          >
             Next
           </button>
         </div>
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const message = [
+  "Learn React",
+  "Apply for jobs",
+  "Invest your new income",
+];
