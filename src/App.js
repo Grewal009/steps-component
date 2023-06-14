d9682782f97ac234a69febb113a888d78772c032
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-14 23:08:52 +0200

    state updated using callback function in setter method

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ const App = () => {
   const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
